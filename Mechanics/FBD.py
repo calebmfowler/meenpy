affd2e1126191a1b98b1ad7689e665f2e05f9890
Caleb Fowler <caleb.fowler.atx@gmail.com>
2025-02-11 12:25:29 -0600

    refactoring names & classes

--- Mechanics/FBD.py
+++ Mechanics/FBD.py
@@ -1,10 +1,3 @@
-class Force:
-    def __init__(self, forceVector, applicationPoint):
-        self.force = forceVector
-        self.point = applicationPoint
-    def __str__(self):
-        return f"{self.force} applied at {self.point}"
-
 class FBD:
     def __init__(self, appliedForceSet, reactionForceSet):
         self.forceSet = appliedForceSet
