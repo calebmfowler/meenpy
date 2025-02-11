0d605ee5755d9a99760ed3c9cae96cd15e335716
Caleb Fowler <caleb.fowler.atx@gmail.com>
2025-02-11 12:33:05 -0600

    refactoring again

--- Mechanics/Force.py
+++
@@ -1,6 +0,0 @@
-class Force:
-    def __init__(self, forceVector, applicationPoint):
-        self.force = forceVector
-        self.point = applicationPoint
-    def __str__(self):
-        return f"{self.force} applied at {self.point}"
